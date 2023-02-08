38c14961dc759eb7c27e6c11c0400c1190f19550
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-08 11:37:23 -0500

    solve for color

--- lib/car.rb
+++ lib/car.rb
@@ -1,11 +1,19 @@
 class Car
-  attr_reader :make, :model, :monthly_payment, :loan_length
+  attr_reader :make, :model, :monthly_payment, :loan_length, :color
 
   def initialize(make_model, monthly_payment, loan_length)
     @make = make_model.split(' ')[0]
     @model = make_model.split(' ')[1]
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color = nil
   end
 
+  def total_cost
+    @monthly_payment * @loan_length
+  end
+
+  def paint!(color)
+    @color = color
+  end
 end
