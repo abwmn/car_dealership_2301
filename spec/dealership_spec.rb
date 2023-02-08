aaae40c7140eb123618ad250fc103f1ef9161ec7
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-08 12:02:49 -0500

    solve for inventory checks

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,51 @@
+require 'rspec'
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  it 'exists' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership).to be_a(Dealership)
+  end
+
+  it 'has an inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+  end
+
+  it 'can add to its inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory).to eq([car_1, car_2])
+  end
+
+  it 'can count its inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory_count).to eq(0)
+
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory_count).to eq(2)
+  end
+
+  it 'can check if it has inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.has_inventory?).to eq(false)
+
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    dealership.add_car(car_1)
+
+    expect(dealership.has_inventory?).to eq(true)
+  end
+end
